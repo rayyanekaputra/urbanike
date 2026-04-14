e99f2d75481022b028a5ac80dc4f54f2b2399dd0
rayyanekaputra <rayyan.ekaputra@gmail.com>
2026-04-14 21:24:46 +0800

    deps: font configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [{
+        provider: fontProviders.local(),
+        name: "Switzer",
+        cssVariable: "--font-switzer",
+        options: {
+            variants: [{
+                src: ['./src/assets/fonts/Switzer-Variable.woff'],
+                weight: '100 900',
+                style: 'normal'
+            }]
+        }
+    }]
+
+
+});
